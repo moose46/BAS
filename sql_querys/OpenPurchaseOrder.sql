38a56a49cce3048838d7dea941f6b415e1f84ad4
me <me@robertcurtiss.com>
2020-03-27 10:43:03 -0400

    Looking for Vendor / Shipping Code Addresses 2

--- sql_querys/OpenPurchaseOrder.sql
+++ sql_querys/OpenPurchaseOrder.sql
@@ -9,16 +9,16 @@ SELECT
 		WHEN (SELECT
 					[Internal ID]
 				FROM NS_Vendors nsv
-				WHERE [External ID] = poh.APDivisionNo + '-' + poh.VendorNo ) is not null
-			 THEN (SELECT
+				WHERE [External ID] = poh.APDivisionNo + '-' + poh.VendorNo)
+			IS NOT NULL THEN (SELECT
 					[Internal ID]
 				FROM NS_Vendors nsv
 				WHERE [External ID] = poh.APDivisionNo + '-' + poh.VendorNo)
-				ELSE
-				(select -314159 )
+		ELSE (SELECT
+					-314159)
 	END AS vendorid
-	,poh.APDivisionNo + '-' + poh.VendorNo as netsuite_external_id
+   ,poh.APDivisionNo + '-' + poh.VendorNo AS netsuite_external_id
 FROM PO_PurchaseOrderHeader poh
-where poh.DateCreated > DATEADD(YEAR, -1,GETDATE())
+WHERE poh.DateCreated > DATEADD(YEAR, -1, GETDATE())
 
-order by vendorid
+ORDER BY vendorid
--- sql_querys/QueryVendorAddress.sql
+++ sql_querys/QueryVendorAddress.sql
@@ -1,7 +1,27 @@
+-- Purchase Order Header Vendor Address vs vendorNo
+
+USE babblefish
+GO
 SELECT
 	ppoh.APDivisionNo
-   ,ppoh.VendorNo,[ShipToAddress1]
+   ,ppoh.VendorNo
+   ,UPPER (PurchaseAddress1)
+   ,ppoh.PurchaseAddress2
+   ,ppoh.PurchaseState
+   ,ppoh.PurchaseCity
+   ,ppoh.PurchaseZipCode
+   ,ppoh.CustomerNo
+   ,ppoh.PurchaseName
 FROM PO_PurchaseOrderHeader ppoh
-WHERE ppoh.DateCreated > DATEADD(YEAR,-1,getdate())
+WHERE ppoh.DateCreated > DATEADD(YEAR, -1, GETDATE())
+--AND ppoh.VendorNo LIKE 'BAU%'
 GROUP BY ppoh.APDivisionNo
-		,ppoh.VendorNo,ppoh.ShipToAddress1
+		,ppoh.VendorNo
+		,ppoh.PurchaseAddress1
+		,ppoh.PurchaseAddress2
+		,ppoh.PurchaseState
+		,ppoh.PurchaseCity
+		,ppoh.PurchaseZipCode
+		,ppoh.CustomerNo
+		,ppoh.PurchaseName
+ORDER BY ppoh.VendorNo
