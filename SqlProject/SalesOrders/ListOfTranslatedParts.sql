b49009f1b9386e76ec335f40767942cb556f75cc
me <me@robertcurtiss.com>
2020-04-07 20:23:23 -0400

    Updates to Parts Translation for PO's

--- SqlProject/SalesOrders/ListOfTranslatedParts.sql
+++ SqlProject/SalesOrders/ListOfTranslatedParts.sql
@@ -4,12 +4,13 @@
 -- rwc 3/24/2020
 -- added warehouse code, change from sod warehousecode to soh warehousecode
 -- sent this to Kathy 4/6/2020 ListOfTransLatedParts.xlsx
+-- Changed 4/7 to help understand what I was doing
 USE babblefish
 GO
 
 
 SELECT distinct
-   sod.ItemCode AS [itemLine_item] -- SO Details
+   sod.ItemCode AS [orginal_partNumber] -- SO Details
    ,CASE
 		WHEN EXISTS (SELECT
 					*
@@ -18,12 +19,22 @@ SELECT distinct
 					xl.NETSUITE
 				FROM ITEMCODE_MAS_NS xl
 				WHERE xl.MAS = REPLACE(sod.ItemCode, '/', ''))
-		ELSE (SELECT
-					netsuite
-				FROM ITEMCODE_MAS_NS
-				WHERE MAS = REPLACE(sod.ItemCode, '/', ''))
+		ELSE (SELECT 'Not In Parts List')
+				--	ITEMCODE_MAS_NS.MAS
+				--FROM ITEMCODE_MAS_NS
+				--WHERE MAS = REPLACE(sod.ItemCode, '/', ''))
 	END AS xlated_part
    ,CASE
+		WHEN EXISTS (SELECT
+					xl.MAS
+				FROM ITEMCODE_MAS_NS xl
+				WHERE xl.MAS = REPLACE(sod.ItemCode, '/', '')) THEN (SELECT
+					xl.NETSUITE
+				FROM ITEMCODE_MAS_NS xl
+				WHERE xl.MAS = REPLACE(sod.ItemCode, '/', ''))
+		ELSE (select '') --REPLACE(sod.ItemCode, '/', '')
+	END AS ns_part
+	   ,CASE
 		WHEN EXISTS (SELECT
 					xl.MAS
 				FROM ITEMCODE_MAS_NS xl
@@ -34,11 +45,12 @@ SELECT distinct
 		ELSE REPLACE(sod.ItemCode, '/', '')
 	END AS final_part
 
+
 FROM [babblefish].[dbo].[SO_SalesOrderHeader] soh
 LEFT JOIN SO_SalesOrderDetail sod
 	ON sod.SalesOrderNo = soh.SalesOrderNo
-LEFT JOIN AR_Customer arc
-	ON arc.CustomerNo = soh.CustomerNo
-WHERE OrderType = 'R' and sod.ItemCode is not null
+--LEFT JOIN AR_Customer arc
+--	ON arc.CustomerNo = soh.CustomerNo
+WHERE OrderType = 'R' and sod.ItemCode is not null and soh.DateCreated >= DATEADD(YEAR,-1,getdate())
 --AND soh.SalesOrderNo LIKE '00715%'
 ORDER BY sod.ItemCode
